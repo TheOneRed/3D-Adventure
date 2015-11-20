d966934a934d0d36fd4b8f1c1e6a1f35035935fc
Jeff Li <jeff.li.cf@gmail.com>
2015-11-20 11:47:41 -0500

    Found Enemy/ Got pick up sound
    Need to do:
    - Make Collider
    - Death

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+
+    private AudioSource[] soundCLips;
+    private AudioSource nothing, star; //SFX ARCADE FREE
+    
+    void Start () {
+        this.soundCLips = gameObject.GetComponents<AudioSource>();
+        this.nothing = soundCLips[0];
+        this.star = soundCLips[1];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Star")
+        {
+            this.star.Play();
+        }
+    }
+
+
+}
