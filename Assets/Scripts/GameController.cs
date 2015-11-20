5cc95e6427a6401697fc76e861079cc5b97ea94d
Jeff Li <jeff.li.cf@gmail.com>
2015-11-20 00:00:40 -0500

    Made Colliders on pickup/ GUI updated
    Need to do:
    - Get enemy
    - Get sounds

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,47 +5,91 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	// PUBLIC INSTANCE VARIABLES
-	public Text scoreLabel;
-	public Text lifeLabel;
+	public Text scoreText;
+	public Text livesText;
+    public Text gameOverText;
+    public Text finalScoreText;
+    public Text restartText;
 
-	// PRIVATE INSTANCE VARIABLES
-	private int _scoreValue = 0;
-	private int _liveValue = 100;
+    // PRIVATE INSTANCE VARIABLES
+    private int scoreValue = 0;
+	private int livesValue = 5;
+    private bool gameOver;
+    private bool restart;
 
-	// PUBLIC PROPERTIES
-	public int Score {
-		get {
-			return _scoreValue;
-		}
-		set {
-			_scoreValue = value;
-			this._updateHUD();
-		}
-	}
 
-	public int Life {
-		get {
-			return _liveValue;
-		}
-		set {
-			_liveValue = value;
-			this._updateHUD();
-		}
-	}
-
-	// Use this for initialization
-	void Start () {
-		this._updateHUD ();
+    // Use this for initialization
+    void Start () {
+        gameOver = false;
+        restart = false;
+        this.gameOverText.enabled = false;
+        this.finalScoreText.enabled = false;
+        this.restartText.enabled = false;
+        this.UpdateLives();
+        this.UpdateScore();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
 
-	// PRIVATE METHODS
-	private void _updateHUD() {
-		this.scoreLabel.text = "Score: " + this._scoreValue;
-		this.lifeLabel.text = "Life: " + this._liveValue;
-	}
+    }
+
+    // When player collects a star
+    public void GainScore(int newScoreValue)
+    {
+        scoreValue += newScoreValue;
+        UpdateScore();
+    }
+
+    // Updates score value from above
+    public void UpdateScore()
+    {
+        scoreText.text = "Score: " + scoreValue;
+    }
+
+    // When player collides with an enemy
+    public void LoseLife(int newLifeValue)
+    {
+        livesValue -= newLifeValue;
+        UpdateLives();
+    }
+
+    // Updates lives value from above
+    public void UpdateLives()
+    {
+        livesText.text = "Lives: " + livesValue;
+
+        if (livesValue == 0)
+        {
+            //playerController.kill(); // Reference to PlayerController to destroy player game object
+            //this.gameOverSound.Play(); //Sound when you hit zero lives value
+            //this.GameOver();
+        }
+    }
+
+    // When lives value hits zero
+    private void GameOver()
+    {
+        gameOver = true;
+        this.scoreText.enabled = false;
+        this.livesText.enabled = false;
+        this.gameOverText.enabled = true;
+        this.finalScoreText.enabled = true;
+        this.finalScoreText.text = "Final Score: " + this.scoreValue;
+
+        //Meassge to press "R" to play again
+        if (gameOver)
+        {
+            this.restartText.enabled = true;
+            this.restartText.text = "Press 'R' to play again";
+            restart = true;
+        }
+    }
 }
---
+++ Assets/Scripts/StarCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarCollider : MonoBehaviour {
+
+    private int points = 100;
+    private GameController gameController;
+
+    // Use this for initialization
+    void Start () {
+
+        // Finding GameController game object to access methods in GameController script 
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameController.GainScore(points);
+            Destroy(gameObject);
+        }
+    }
+}
